e18c26a0755b447dc65c531553f92f5425350ddd
Jomer John Alvarado <159941293+SyntaxRepo@users.noreply.github.com>
2025-08-03 09:47:55 -0700

    Update webinfo.py

--- webinfo.py
+++ webinfo.py
@@ -15,7 +15,7 @@ HTML_HOMEPAGE_TEMPLATE = """
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Website Info Scanner - Home</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
