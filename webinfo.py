14eb24104d94ae0576c285f53467073377e0e848
Jomer John Alvarado <159941293+SyntaxRepo@users.noreply.github.com>
2025-08-09 14:05:34 +0800

    Update webinfo.py

--- webinfo.py
+++ webinfo.py
@@ -20,7 +20,7 @@ HTML_HOMEPAGE_TEMPLATE = """
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="manifest.json">
     <script>
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
